61033ad338b66380b8222109d29ebeb170957d73
Marshall Bowers <elliott.codes@gmail.com>
2022-10-29 12:37:07 -0400

    Scaffold `LinearClient`

---
+++ src/client.rs
@@ -0,0 +1,72 @@
+use url::{ParseError, Url};
+
+use crate::ApiKey;
+
+/// The Linear client.
+pub struct LinearClient {
+    base_url: Url,
+    key: ApiKey,
+    client: reqwest::Client,
+}
+
+impl LinearClient {
+    /// Returns a new instance of the Linear client using the provided API key.
+    pub fn new(key: &ApiKey) -> Self {
+        LinearClientBuilder::new(key).build()
+    }
+
+    /// Returns a [`LinearClientBuilder`] that may be used to construct a Linear client.
+    pub fn builder(key: &ApiKey) -> LinearClientBuilder {
+        LinearClientBuilder::new(key)
+    }
+
+    pub(crate) fn base_url(&self) -> &Url {
+        &self.base_url
+    }
+
+    pub(crate) fn key(&self) -> &ApiKey {
+        &self.key
+    }
+}
+
+/// A builder for a Linear client.
+pub struct LinearClientBuilder<'a> {
+    base_url: Url,
+    key: &'a ApiKey,
+}
+
+impl<'a> LinearClientBuilder<'a> {
+    /// Returns a new [`LinearClientBuilder`] using the provided API key.
+    pub fn new(key: &'a ApiKey) -> Self {
+        Self {
+            base_url: Url::parse("https://api.linear.app/graphql").unwrap(),
+            key,
+        }
+    }
+
+    /// Sets the base URL of the Linear API that the client should point to.
+    pub fn base_url(mut self, base_url: &'a str) -> Result<LinearClientBuilder, ParseError> {
+        self.base_url = Url::parse(base_url)?;
+        Ok(self)
+    }
+
+    /// Sets the API key that the client will use.
+    pub fn key(mut self, key: &'a ApiKey) -> Self {
+        self.key = key;
+        self
+    }
+
+    /// Consumes the builder and returns the constructed client.
+    pub fn build(self) -> LinearClient {
+        let client = reqwest::Client::builder()
+            .user_agent(concat!("linear_sdk/", env!("CARGO_PKG_VERSION")))
+            .build()
+            .unwrap();
+
+        LinearClient {
+            base_url: self.base_url,
+            key: self.key.to_owned(),
+            client,
+        }
+    }
+}
---
+++ src/core/types.rs
@@ -0,0 +1,3 @@
+mod api_key;
+
+pub use api_key::*;
